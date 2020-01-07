e298ae73debb37687986654a43528ff1221e7149
Menighin <joao.menighin@gmail.com>
2020-01-06 23:05:10 -0300

    Going to stocks page

--- src/lib/CeiCrawler.js
+++ src/lib/CeiCrawler.js
@@ -23,6 +23,8 @@ class CeiCrawler {
     }
 
     async _login() {
+        if (this._isLogged) return;
+
         if (this._browser == null)
             this._browser = await puppeteer.launch({ headless: false });
 
@@ -32,6 +34,41 @@ class CeiCrawler {
         await this._page.type('#ctl00_ContentPlaceHolder1_txtSenha', this.password, { delay: 10 });
         await this._page.click('#ctl00_ContentPlaceHolder1_btnLogar');
         await this._page.waitForNavigation();
+        this._isLogged = true;
+    }
+
+    async getStockHistory() {
+        await this._login();
+        await this._page.goto('https://cei.b3.com.br/CEI_Responsivo/negociacao-de-ativos.aspx');
+
+        const institutionsHandle = await this._page.evaluateHandle(() => {
+            return Array.from(document.querySelectorAll('#ctl00_ContentPlaceHolder1_ddlAgentes option'))
+                .map(o => o.value)
+                .filter(v => v > 0);
+        });
+        const institutions = await institutionsHandle.jsonValue();
+
+        for (const value of institutions) {
+            await this._page.select('#ctl00_ContentPlaceHolder1_ddlAgentes', value);
+
+            //await this._page.waitForNavigation({ waitUntil: 'networkidle0'});
+
+            const accountsHandle = await this._page.evaluateHandle(() => {
+                return Array.from(document.querySelectorAll('#ctl00_ContentPlaceHolder1_ddlContas option'))
+                    .map(o => o.value)
+                    .filter(v => v > 0);
+            });
+            const accounts = await accountsHandle.jsonValue();
+            
+            console.log(accounts);
+
+            await this._page.waitFor(2000);
+        }
+
+
+
+        await this._page.waitFor(10000);
+
     }
 
 }
