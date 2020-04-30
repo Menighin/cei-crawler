95ba7e53a952484bc82003d93e50830fca4a050e
Menighin <joao.menighin@gmail.com>
2020-04-29 23:26:20 -0300

    Configuring distribution

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+let config = {
+  entry: './src/app.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
+module.exports = config;
