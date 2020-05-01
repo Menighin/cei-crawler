0fae2c61bdc1e499fb49c4a8d2600d826948ed2b
Menighin <joao.menighin@gmail.com>
2020-05-01 01:04:31 -0300

    Fix Travis deploy - Adding README

--- before_deploy.js
+++ before_deploy.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
+// Copying and modifying package
 const package = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 package.main = 'app.js';
 
 fs.writeFileSync('dist/package.json', JSON.stringify(package, null, '\t'));
+
+// Copying README file
+fs.writeFileSync('dist/README.md', fs.readFileSync('README.md'));
