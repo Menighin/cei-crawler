706353395a9fbf436040aee5f36197ad347ee0ac
Menighin <joao.menighin@gmail.com>
2020-08-23 00:42:47 -0300

    Sponsorship & Advertisement

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const CeiCrawler = require('./lib/CeiCrawler');
 const { CeiCrawlerError, CeiErrorTypes } = require('./lib/CeiCrawlerError')
 
+const ceiCrawler = new CeiCrawler('01603772693', '#sddsCei1', { puppeteerLaunch: { headless: false }, trace: true });
+ceiCrawler.getWallet().then(console.log);
+
 module.exports = CeiCrawler;
 module.exports.CeiCrawlerError = CeiCrawlerError;
 module.exports.CeiErrorTypes = CeiErrorTypes;
