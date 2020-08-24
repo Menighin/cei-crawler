cff99bc21c5dd371a2e5f2074f2a85d33bf8faff
Menighin <joao.menighin@gmail.com>
2020-08-23 23:13:05 -0300

    Fixing bad commit -.-

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const CeiCrawler = require('./lib/CeiCrawler');
+const { CeiCrawlerError, CeiErrorTypes } = require('./lib/CeiCrawlerError')
+
+module.exports = CeiCrawler;
+module.exports.CeiCrawlerError = CeiCrawlerError;
+module.exports.CeiErrorTypes = CeiErrorTypes;
